8e751ffa5688b23744ee666e4486de8858d48b3c
we6jbo <we6jbo@gmail.com>
2015-10-25 14:30:02 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <body>
 
 <a href="begin.php">Please click here.</a><p>
+<p><a href="more.php">More options (Updated every week!)</a></p>
 If you notice any problems what so ever, please email them to we6jbo@gmail.com!
 </body>
 </html>
