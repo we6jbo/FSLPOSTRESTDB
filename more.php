debb61cf1f54247e9bc573dd666978f3eaf9ff6e
we6jbo <we6jbo@gmail.com>
2015-10-25 14:30:32 -0700

    Create more.php

---
+++ more.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Options</title>
+</head>
+<body>
+<p>This is where I&#39;ll add links to additional options that you can use to access the database. Please expect these options to change frequently!</p>
+<p><a href="restv1.php">Added Oct 25 2015 - More info to come.</a></p>
+
+<p><a href="https://github.com/we6jbo/FSLPOSTRESTDB">Source code</a></p>
+
+<p><strong>Please report any problems you find to we6jbo@gmail.com</strong></p>
+
+  </body>
+</html>
