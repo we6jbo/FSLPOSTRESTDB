ddf8fc58f905f344c3ce87a2b66c6735145bce47
we6jbo <we6jbo@gmail.com>
2015-10-24 21:53:22 -0700

    Create begin.php

---
+++ begin.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Write</title>
+</head>
+<body>
+<form action="write.php" id="form1" method="post" name="form1">
+<p>Name: <input id="name" name="name" size="40" type="text" /></p>
+
+<p>Email:<input id="email" name="email" size="40" type="text" /></p>
+
+<p>Comment:<textarea cols="40" id="comment" name="comment" rows="3"></textarea></p>
+
+<p><input name="Submit" type="submit" value="Submit" /></p>
+
+<p><a href="read.php">Read database</a></p>
+</form>
+</body>
+</html>
